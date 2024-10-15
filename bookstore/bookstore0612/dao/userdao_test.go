03098d00eb3fb09e7d391bf8f3051e5e19f0e021
CongchengC <06chengcong@163.com>
2024-10-15 22:19:11 +0800

    数据库-utils-model-dao 链路验证

--- bookstore/bookstore0612/dao/userdao_test.go
+++ bookstore/bookstore0612/dao/userdao_test.go
@@ -9,7 +9,7 @@ func TestUser(t *testing.T) {
 	fmt.Println("测试userdao中的函数")
 	t.Run("验证用户名或密码：", testLogin)
 	t.Run("验证用户名：", testRegist)
-	// t.Run("保存用户：", testSave)
+	t.Run("保存用户：", testSave)
 }
 
 func testLogin(t *testing.T) {
